d661d68f5ee42560b4a5afca70b05f6777b08840
Fabián Reyes <fabianreyesmn@gmail.com>
2025-07-12 12:03:17 -0600

    Vista de salas

--- src/app/features/salas/pages/listar.ts
+++ src/app/features/salas/pages/listar.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, Signal, WritableSignal, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SalasService } from '../../../core/services/salas';
 
 @Component({
   selector: 'app-listar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './listar.html',
-  styleUrl: './listar.scss'
+  styleUrls: ['./listar.scss']
 })
-export class Listar {
+export class Listar implements OnInit {
+  private salasService = inject(SalasService);
 
+  salas: WritableSignal<any[]> = signal([]);
+
+  ngOnInit(): void {
+    this.cargarSalas();
+  }
+
+  async cargarSalas() {
+    try {
+      const res = await this.salasService.getSalas();
+      this.salas.set(res.data);
+    } catch (error) {
+      console.error('Error al cargar salas', error);
+    }
+  }
 }
