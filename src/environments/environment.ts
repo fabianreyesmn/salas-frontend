0eb6989d3a432772ac88796fae2de78b1d857c23
Fabián Reyes <fabianreyesmn@gmail.com>
2025-07-14 23:51:37 -0600

    URL del backend para produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // Configuración del entorno de desarrollo
-  production: false,
-  apiUrl: 'http://localhost:3000/api'
+  production: true,
+  apiUrl: 'salas-backend-production.up.railway.app'
 };
